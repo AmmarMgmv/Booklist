ade214d9f5791c1006afccf6fba2e5f8051ef266
AmmarMgmv <ammarmgmv@gmail.com>
2022-09-20 14:24:39 +0100

    Destructured props, added description, updated css

--- src/index.js
+++ src/index.js
@@ -27,29 +27,72 @@ function Booklist(){
         img={firstBook.img}
         title={firstBook.title}
         author={firstBook.author}
-      />
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing 
+          elit. Deserunt, excepturi? Libero exercitationem unde 
+          rem aspernatur eius voluptatum saepe enim mollitia.
+        </p>
+      </Book>
+
       <Book
         img={secondBook.img}
         title={secondBook.title}
         author={secondBook.author}
-      />
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing 
+          elit. Suscipit sit aspernatur optio ut cum illum 
+          expedita, et ipsa exercitationem corrupti?
+        </p>
+      </Book>
+
       <Book
         img={thirdBook.img}
         title={thirdBook.title}
         author={thirdBook.author}
-      />
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Consequuntur ducimus ex eveniet placeat illo, nisi eum 
+          cum quisquam culpa est.
+        </p>
+      </Book>
     </section>
   )
 }
 
+// const Book = ({img, title, author, children}) => {
+//   return (
+//     <article className='book'>
+//       <img className='cover' src={img} alt="" />
+//       <h2 className='title'>{title}</h2>
+//       <h4 className='author'>{author}</h4>
+//       {children}  
+//     </article>
+//   )
+// }
+
 const Book = (props) => {
+  const {img, title, author, children} = props
   return (
     <article className='book'>
-      <img className='cover' src={props.img} alt="" />
-      <h2 className='title'>{props.title}</h2>
-      <h4 className='author'>{props.author}</h4>
+      <img className='cover' src={img} alt="" />
+      <h2 className='title'>{title}</h2>
+      <h4 className='author'>{author}</h4>
+      {children}
     </article>
   )
 }
+
+// const Book = (props) => {
+//   return (
+//     <article className='book'>
+//       <img className='cover' src={props.img} alt="" />
+//       <h2 className='title'>{props.title}</h2>
+//       <h4 className='author'>{props.author}</h4>
+//     </article>
+//   )
+// }
 
 ReactDom.render(<Booklist/>, document.getElementById('root'));
