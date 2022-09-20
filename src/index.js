5ab86ee83216fe401f95bfbe01a25746348c1170
AmmarMgmv <ammarmgmv@gmail.com>
2022-09-20 14:26:55 +0100

    Refactored using lists, updated code for React18

--- src/index.js
+++ src/index.js
@@ -1,98 +1,51 @@
-import ReactDom from 'react-dom';
+import ReactDom from 'react-dom/client';
 
 import './index.css'
 
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1647789287i/60177373.jpg',
-  title: 'Fairy Tale',
-  author: 'Stephen King'
-}
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1647789287i/60177373.jpg',
+    title: 'Fairy Tale',
+    author: 'Stephen King',
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis deserunt expedita aperiam inventore reiciendis rerum delectus!'
+  },
+  
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1373903563i/11566.jpg',
+    title: 'The Green Mile',
+    author: 'Stephen King',
+    desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum fugiat asperiores magni laudantium dolorum! Pariatur necessitatibus?'
+  },
 
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1373903563i/11566.jpg',
-  title: 'The Green Mile',
-  author: 'Stephen King'
-}
-
-const thirdBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1213131305i/149267.jpg',
-  title: 'The Stand',
-  author: 'Stephen King'
-}
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1213131305i/149267.jpg',
+    title: 'The Stand',
+    author: 'Stephen King',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus ut cumque nobis ab. Rerum modi laudantium ex itaque facilis odio.'
+  }
+]
 
 function Booklist(){
   return (
     <section className='booklist'>
-      <Book 
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing 
-          elit. Deserunt, excepturi? Libero exercitationem unde 
-          rem aspernatur eius voluptatum saepe enim mollitia.
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing 
-          elit. Suscipit sit aspernatur optio ut cum illum 
-          expedita, et ipsa exercitationem corrupti?
-        </p>
-      </Book>
-
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Consequuntur ducimus ex eveniet placeat illo, nisi eum 
-          cum quisquam culpa est.
-        </p>
-      </Book>
+      {books.map((book) => {
+        return <Book book={book}></Book>;
+      })}
     </section>
   )
 }
 
-// const Book = ({img, title, author, children}) => {
-//   return (
-//     <article className='book'>
-//       <img className='cover' src={img} alt="" />
-//       <h2 className='title'>{title}</h2>
-//       <h4 className='author'>{author}</h4>
-//       {children}  
-//     </article>
-//   )
-// }
-
 const Book = (props) => {
-  const {img, title, author, children} = props
+  const {img, title, author, desc} = props.book
   return (
     <article className='book'>
       <img className='cover' src={img} alt="" />
       <h2 className='title'>{title}</h2>
       <h4 className='author'>{author}</h4>
-      {children}
+      <p className='description'>{desc}</p>
     </article>
   )
 }
 
-// const Book = (props) => {
-//   return (
-//     <article className='book'>
-//       <img className='cover' src={props.img} alt="" />
-//       <h2 className='title'>{props.title}</h2>
-//       <h4 className='author'>{props.author}</h4>
-//     </article>
-//   )
-// }
-
-ReactDom.render(<Booklist/>, document.getElementById('root'));
+const root = ReactDom.createRoot(document.getElementById('root'));
+root.render(<Booklist></Booklist>)
