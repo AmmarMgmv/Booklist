13c87baade554b0109de75e493e3ee2619911ea0
AmmarMgmv <ammarmgmv@gmail.com>
2022-09-20 14:22:57 +0100

    Cleaned code using props, updated book selection

--- src/index.js
+++ src/index.js
@@ -2,42 +2,54 @@ import ReactDom from 'react-dom';
 
 import './index.css'
 
+const firstBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1647789287i/60177373.jpg',
+  title: 'Fairy Tale',
+  author: 'Stephen King'
+}
+
+const secondBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1373903563i/11566.jpg',
+  title: 'The Green Mile',
+  author: 'Stephen King'
+}
+
+const thirdBook = {
+  img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1213131305i/149267.jpg',
+  title: 'The Stand',
+  author: 'Stephen King'
+}
+
 function Booklist(){
   return (
     <section className='booklist'>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
-      <Book/>
+      <Book 
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book
+        img={thirdBook.img}
+        title={thirdBook.title}
+        author={thirdBook.author}
+      />
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className='book'>
-      <Image/>
-      <Title/>
-      <Author/>
+      <img className='cover' src={props.img} alt="" />
+      <h2 className='title'>{props.title}</h2>
+      <h4 className='author'>{props.author}</h4>
     </article>
   )
-}
-
-const Image = () => {
-  return <img className='cover' src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1647789287i/60177373.jpg" alt=""/>
-}
-
-const Title = () => {
-  return <h2 className='title'>Fairy Tale: Stephen King</h2>
-}
-
-const Author = () => {
-  return <h4 className='author'>Stephen King</h4>
 }
 
 ReactDom.render(<Booklist/>, document.getElementById('root'));
