04ded094c8370325a94ad4e4edd7dcb157986e24
AmmarMgmv <ammarmgmv@gmail.com>
2022-09-21 02:57:52 +0100

    Added onClick & onMouseOver events

--- src/index.js
+++ src/index.js
@@ -40,12 +40,22 @@ function Booklist(){
 
 const Book = (props) => {
   const {img, title, author, desc} = props
+
+  const clickHandler =() => {
+    alert('Hello World');
+  }
+  const complexExample = (author) => {
+console.log(author);
+  }
+
   return (
-    <article className='book'>
+    <article className='book' onMouseOver={() => {console.log(title)}}>
       <img className='cover' src={img} alt="" />
-      <h2 className='title'>{title}</h2>
+      <h2 onClick={() => console.log(title)} className='title'>{title}</h2>
       <h4 className='author'>{author}</h4>
       <p className='description'>{desc}</p>
+      <button type="button" onClick={clickHandler}>Reference Example</button>
+      <button type="button" onClick={() => complexExample(author)}>More Complex Example</button>
     </article>
   )
 }
